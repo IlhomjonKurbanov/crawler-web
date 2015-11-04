04dae37ae46e1b3e4c051de5ecd45002596e15aa
Hai Nguyen <duchaiweb@gmail.com>
2015-11-04 09:16:49 +0700

    Add 4 coner points to table Venues

--- app/Venue.php
+++ app/Venue.php
@@ -16,7 +16,7 @@ class Venue extends Model {
      *
      * @var array
      */
-    protected $fillable = ['name', 'description', 'venue_type_id', 'gps_latitude', 'gps_longitude', 'zip_postal_code'];
+    protected $fillable = ['name', 'description', 'venue_type_id', 'gps_latitude', 'gps_longitude', 'zip_postal_code', 'lat_top_left', 'lng_top_left', 'lat_top_right', 'lng_top_right', 'lat_bottom_left', 'lng_bottom_left', 'lat_bottom_right', 'lng_bottom_right'];
 
     /**
      * The attributes excluded from the model's JSON form.
---
+++ database/migrations/2015_11_04_020148_add_coner_points_to_venue_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Venue;
+
+class AddConerPointsToVenueTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('venues', function($table)
+		{
+			$table->string('lat_top_left');
+			$table->string('lng_top_left');
+
+			$table->string('lat_top_right');
+			$table->string('lng_top_right');
+
+			$table->string('lat_bottom_left');
+			$table->string('lng_bottom_left');
+
+			$table->string('lat_bottom_right');
+			$table->string('lng_bottom_right');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('venues', function ($table) {
+		    $table->dropColumn(['lat_top_left', 'lng_top_left', 'lat_top_right', 'lng_top_right', 'lat_bottom_left', 'lng_bottom_left', 'lat_bottom_right', 'lng_bottom_right']);
+		});
+	}
+
+}
