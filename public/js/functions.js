d89657b8005d307ef48643228e7034eed7de9ff6
huynt <huynt57@gmail.com>
2015-11-04 10:03:32 +0700

    draw rectangle

--- public/js/functions.js
+++ public/js/functions.js
@@ -14,6 +14,26 @@ $(document).ready(function () {
             placeMarker(ll, 'poi');
         }
     });
+
+    $("#draggable-3").draggable({helper: 'clone',
+        stop: function (e) {
+            var NorthEast = new google.maps.Point(e.pageX + 60, e.pageY + 60);
+            var NorthWest = new google.maps.Point(e.pageX - 60, e.pageY + 60);
+            var SouthWest = new google.maps.Point(e.pageX - 60, e.pageY - 60);
+            var SouthEast = new google.maps.Point(e.pageX + 60, e.pageY - 60);
+
+            var NEBounds = overlay.getProjection().fromContainerPixelToLatLng(NorthEast);
+            var NWBounds = overlay.getProjection().fromContainerPixelToLatLng(NorthWest);
+            var SWBounds = overlay.getProjection().fromContainerPixelToLatLng(SouthWest);
+            var SEBounds = overlay.getProjection().fromContainerPixelToLatLng(SouthEast);
+            //var latlngBounds = new google.maps.LatLngBounds(NorthEast, SouthWest);
+            //console.log(NEBounds.lat());
+            //console.log(NorthEast.lng());
+            //console.log(latlngBounds.getNorthEast().lat());
+            //var ll = overlay.getProjection().get(latlngBounds);
+            placeRectangle(NEBounds.lat(), SEBounds.lat(), SEBounds.lng(), SWBounds.lng(), 'rec');
+        }
+    });
 });
 
 var $map;
@@ -65,62 +85,104 @@ function placeMarker(location, type) {
     {
         color = '#0000FF';
     }
-    var cityCircle = new google.maps.Circle({
-        strokeColor: color,
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: color,
-        fillOpacity: 0.35,
-        map: $map,
-        //position: location,
-        center: location,
-        radius: 60,
-        editable: true,
-        draggable: true,
-    });
-    cityCircle.set('id', cuniq());
-    cityCircle.set('type', type);
-    saveCirle(cityCircle);
-    google.maps.event.addListener(cityCircle, 'center_changed', function ()
+    if (type === 'node' || type === 'poi')
     {
-        //console.log(cityCircle.getCenter());
-    });
-    google.maps.event.addListener(cityCircle, 'radius_changed', function ()
-    {
+        var cityCircle = new google.maps.Circle({
+            strokeColor: color,
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: color,
+            fillOpacity: 0.35,
+            map: $map,
+            //position: location,
+            center: location,
+            radius: 60,
+            editable: true,
+            draggable: true,
+        });
+        cityCircle.set('id', cuniq());
+        cityCircle.set('type', type);
         saveCirle(cityCircle);
-    });
-    google.maps.event.addListener(cityCircle, 'dragend', function ()
-    {
+        google.maps.event.addListener(cityCircle, 'center_changed', function ()
+        {
+            //console.log(cityCircle.getCenter());
+        });
+        google.maps.event.addListener(cityCircle, 'radius_changed', function ()
+        {
+            saveCirle(cityCircle);
+        });
+        google.maps.event.addListener(cityCircle, 'dragend', function ()
+        {
 
-        saveCirle(cityCircle);
-    });
-
-    function saveCirle(cityCircle)
-    {
-        var lat = cityCircle.getCenter().lat();
-        var lng = cityCircle.getCenter().lng();
-        var radius = cityCircle.getRadius();
-        var id = cityCircle.get('id');
-        var type = cityCircle.get('type');
-        console.log(lat);
-        console.log(lng);
-        console.log(radius);
-        console.log(id);
-        console.log(type);
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost/crawler/public/saveCircle',
-            data: {
-                lat: lat, lng: lng, radius: radius, id: id, type: type
-            },
-            success: function (response)
-            {
-
-            },
-            error: function ()
-            {
-
-            }
+            saveCirle(cityCircle);
         });
     }
 }
+
+function placeRectangle(north, south, east, west, type)
+{
+    console.log(north, south, east, west);
+    var cityRectangle = new google.maps.Rectangle({
+        strokeColor: '#008000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#008000',
+        fillOpacity: 0.35,
+        map: $map,
+        //position: location,
+        bounds: {
+            north: north,
+            south: south,
+            east: east,
+            west: west,
+        },
+        //radius: 60,
+        editable: true,
+        draggable: true,
+    });
+    cityRectangle.set('id', cuniq());
+    cityRectangle.set('type', type);
+    // saveCirle(cityCircle);
+    google.maps.event.addListener(cityRectangle, 'bounds_changed', function ()
+    {
+        //console.log(cityCircle.getCenter());
+    });
+
+    google.maps.event.addListener(cityRectangle, 'dragend', function ()
+    {
+
+        //saveCirle(cityCircle);
+        console.log(cityRectangle.getBounds());
+    });
+
+}
+
+function saveCirle(cityCircle)
+{
+    var lat = cityCircle.getCenter().lat();
+    var lng = cityCircle.getCenter().lng();
+    var radius = cityCircle.getRadius();
+    var id = cityCircle.get('id');
+    var type = cityCircle.get('type');
+    console.log(lat);
+    console.log(lng);
+    console.log(radius);
+    console.log(id);
+    console.log(type);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost/crawler/public/saveCircle',
+        data: {
+            lat: lat, lng: lng, radius: radius, id: id, type: type
+        },
+        success: function (response)
+        {
+
+        },
+        error: function ()
+        {
+
+        }
+    });
+}
+
--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html>
-  <head>
-  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-  <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-  <script src="https://hpneo.github.io/gmaps/gmaps.js"></script>
-  <script src="{{asset('js/functions.js')}}"></script>
-  <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
-  </head>
-  <body onload="initialize()">
-    <div id="map_canvas">    </div>
-    <div class='shelf' id='shelf-1'><img class="draggable" id="draggable-1" src='{{asset('img/icon.png')}}'/><div>
-    <div class="shelf" id='shelf-2'><img class="draggable" id="draggable-2" src='{{asset('img/icon2.png')}}'/><div>
-  </body>
+    <head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+        <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+        <script src="https://hpneo.github.io/gmaps/gmaps.js"></script>
+        <script src="{{asset('js/functions.js')}}"></script>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+    </head>
+    <body onload="initialize()">
+        <div id="map_canvas">    </div>
+        <div class='shelf' id='shelf-1'><img class="draggable" id="draggable-1" src='{{asset('img/icon.png')}}'/></div>
+        <div class="shelf" id='shelf-2'><img class="draggable" id="draggable-2" src='{{asset('img/icon2.png')}}'/></div>
+        <div class="shelf" id='shelf-3'><img class="draggable" id="draggable-3" src='{{asset('img/icon3.png')}}'/></div>    
+    </body>
 </html>
