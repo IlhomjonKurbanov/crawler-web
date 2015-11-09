e3d26492b1f5aa4a24bd5f4d254bb9c0fceed2d3
huynt <huynt57@gmail.com>
2015-11-09 11:48:29 +0700

    place an image

--- public/js/functions.js
+++ public/js/functions.js
@@ -75,8 +75,22 @@ function initialize() {
 
     overlay = new google.maps.OverlayView();
     overlay.draw = function () {
+
     };
     overlay.setMap($map);
+
+    var imageBounds = {
+        north: 40.773941,
+        south: 40.712216,
+        east: -74.12544,
+        west: -74.22655
+    };
+
+    historicalOverlay = new google.maps.GroundOverlay(
+            'https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg',
+            imageBounds);
+    historicalOverlay.setMap($map);
+
 }
 
 function disableAllRectangle()
